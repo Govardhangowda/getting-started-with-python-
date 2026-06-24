d0f684f3c405f6660de1297f1ae03c6bcc54caf3
Govardhangowda <66202233+Govardhangowda@users.noreply.github.com>
2026-06-24 14:57:17 +0000

    this was day 5

---
+++ list.py
@@ -0,0 +1,8 @@
+s=['apple','orange','banana','chickoo','watermelon']
+print(s)
+s.append('dragon fruit')
+print(s)
+s.insert(1,'mangoo')
+print(s)
+s.pop(2)
+print(s)
---
+++ sortlist.py
@@ -0,0 +1,4 @@
+s=[3,2,4,1,9]
+g=sorted(s,reverse=True)
+print(g)
+print(g[::-1])
