3e03183fcceb6c45e694f47c2843b9205624cdda
Govardhangowda <66202233+Govardhangowda@users.noreply.github.com>
2026-06-26 21:28:40 +0530

    Create eat.py

---
+++ eat.py
@@ -0,0 +1,9 @@
+time=input('what is the time:?').strip().upper()
+if time==('8AM'):
+    print('Breakfast')
+elif time==('1PM'):
+    print('Lunch')
+elif time==('8PM'):
+    print('Dinner')
+else:
+    print('it is not time for dinner')
