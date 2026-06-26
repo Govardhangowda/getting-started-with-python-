fb5d1e59376e228d0d1d066d82e7cfc173e1fc4a
Govardhangowda <66202233+Govardhangowda@users.noreply.github.com>
2026-06-26 20:12:07 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+dict={'shivamogga':'kadabu','mangaluru':'neerdose','bengaluru':'bisbelebath','mysuru':'mysore pak','udupi':'sukka'}
+print(dict)
+dict['hubli']=['girmit']
+print(dict)
+dict['bengaluru']=['bellulikabab']
+print(dict)
+dict.pop('mysuru')
+print(dict.keys())
+print(dict.values())
