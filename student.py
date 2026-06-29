e8a5cfcbae3c6c54a197f386d9b461f2beb1b21a
Govardhangowda <66202233+Govardhangowda@users.noreply.github.com>
2026-06-29 22:13:55 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,13 @@
+students=[
+    {
+        'name':'david',
+        'age':'17',
+        'score':'28'
+    },
+    {
+        'name':'salt',
+        'age':'18',
+        'score':'56'
+    }]
+for student in students:
+    print(student)
