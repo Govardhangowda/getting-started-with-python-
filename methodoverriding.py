0f2b7a963b76aa0fa9c7f8e6619852f054c5a4c6
Govar <66202233+Govardhangowda@users.noreply.github.com>
2026-07-05 19:52:02 +0530

    Create methodoverriding.py

---
+++ methodoverriding.py
@@ -0,0 +1,7 @@
+class calculator:
+    def multiply(self,a,b,c=1):
+        print(f'The result of multiplication is : {a*b*c}')
+        
+clct1=calculator()
+clct1.multiply(2,3)
+clct1.multiply(2,3,4)
