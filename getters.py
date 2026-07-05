2eaebef024b6b6abaf74ad3df505f109238aa783
Govar <66202233+Govardhangowda@users.noreply.github.com>
2026-07-05 19:34:09 +0530

    Update getters.py

--- getters.py
+++ getters.py
@@ -13,4 +13,8 @@ class bankaccount:
         else:
             self.balance=self.balance+update
             print(f'The balnce now is: {self.balance}')
+
+acnt1=bankaccount('1001',2000)
+print(f'The balance is: {acnt1.get_balance()}')
+acnt1.updatebalance()
             
