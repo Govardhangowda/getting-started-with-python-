7f94df75778786abe50954d2c1585d23a56a907b
Govardhan Gowda <govardhangowda2007@gmail.com>
2026-07-11 09:12:42 +0530

    update readlines.py

--- readlines.py
+++ readlines.py
@@ -1,7 +1,10 @@
 j=0
 js=input("Enter the name of the file to read: ")
-with open(js, "r") as file:
-    lines = file.readlines()
-    for line in lines:
-        j += 1
-print(f"Number of lines in the file: {j}")
+try:
+    with open(js, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            j += 1
+    print(f"Number of lines in the file: {j}")
+except FileNotFoundError:
+    print("File not found.")
